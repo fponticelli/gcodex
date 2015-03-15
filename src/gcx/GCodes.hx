9f046cc31bca578519d8bcd4739b8a2090f5cfff
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-15 16:03:25 -0600

    fixed issue with hole (?)

--- src/gcx/GCodes.hx
+++ src/gcx/GCodes.hx
@@ -4,15 +4,20 @@ import gcx.AddressType;
 
 class GCodes {
   public static function hole(d : GCodeDriver, toolDiameter : Float, holeDiameter : Float, ?overlap = 0.1) {
-    var step = toolDiameter * (1 - overlap),
+
+    var toolRadius = toolDiameter / 2,
+        holeRadius = holeDiameter / 2,
+        step = toolDiameter * (1 - overlap),
         dist = step,
-        rad  = holeDiameter / 2 - toolDiameter;
+        rad  = holeRadius - toolRadius;
+    if(rad <= toolRadius)
+      return;
     while(dist < rad) {
       d.linear([X(-step)]);
       d.arc([dist, 0], [0, 0]);
       dist += step;
     }
-    d.linear([X(-rad)]);
+    d.linear([X(rad - (dist - step))]);
     d.arc([rad, 0], [0, 0]);
   }
 }
