661bfa664017c833c5b49c6022ffe8c088251d1c
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-15 15:48:59 -0600

    added rounding

--- src/gcx/Address.hx
+++ src/gcx/Address.hx
@@ -1,14 +1,20 @@
 package gcx;
 
+using thx.core.Floats;
+
 abstract Address(AddressType) from AddressType to AddressType {
   @:to public function toString() : String
     return switch this {
-      case X(v): 'X$v';
-      case Y(v): 'Y$v';
-      case Z(v): 'Z$v';
-      case I(v): 'I$v';
-      case J(v): 'J$v';
-      case K(v): 'K$v';
-      case P(v): 'P$v';
+      case X(v): 'X${r(v)}';
+      case Y(v): 'Y${r(v)}';
+      case Z(v): 'Z${r(v)}';
+      case I(v): 'I${r(v)}';
+      case J(v): 'J${r(v)}';
+      case K(v): 'K${r(v)}';
+      case P(v): 'P${r(v)}';
     };
+
+  static function r(f : Float) : String {
+    return ""+f.roundTo(8);
+  }
 }
