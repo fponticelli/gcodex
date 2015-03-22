c1773d6d5e51c27af722f135697aafa37e1cfd53
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-21 22:38:42 -0600

    moved samples to individual files and resized

---
+++ demo/Clamp.hx
@@ -0,0 +1,59 @@
+import gcx.Pointer;
+using thx.core.Floats;
+
+class Clamp {
+  public static function build(po : Pointer) {
+		var m = 5,
+        w = 24,
+        sx = m + w / 2,
+        sy = m + w / 2,
+        o = 5,
+        materialHeight = 25.4 / 4,
+        passes = 3,
+        cutDepth = -(materialHeight / passes * 1.1).ceilTo(2),
+        emD = 25.4 / 8,
+        emR = emD / 2,
+        holeD = 8.0,
+        holeR = holeD / 2,
+        cutOff = 15,
+        length = 150,
+        mo = o - emR,
+        holes = 4,
+        cutL = (length - cutOff * (holes - 1)) / holes;
+
+    // go to first hole
+    po.z(o)
+      .abs(sx, sy);
+
+    // make n holes
+    for(i in 0...holes) {
+      // mill big cut
+      po.y(sy + (cutOff + cutL) * i)
+        .z(0)
+        .mill(1200);
+      for(_ in 1...passes+1) {
+        po.rz(cutDepth)
+          .yHole(emD, holeD, cutL);
+      }
+      po.travel()
+        .z(o);
+    }
+
+
+    // cut profile
+    var pw = w / 2 + emR,
+        px = sx - pw;
+    po.abs(px, sy)
+      .z(0)
+      .mill();
+    for(_ in 1...passes+1) {
+      po.rz(cutDepth)
+        .ry(length)
+        .rarc(pw, 0, pw * 2, 0)
+        .ry(-length)
+        .rarc(-pw, 0, -pw * 2, 0);
+    }
+    po.travel()
+      .z(o);
+  }
+}
---
+++ demo/Plate.hx
@@ -0,0 +1,60 @@
+import gcx.Pointer;
+using thx.core.Floats;
+
+class Plate {
+  public static function build(po : Pointer) {
+    var w = 80,
+        h = w,
+        o = 5,
+        mill = 150,
+        passes = 3,
+        material = 3,
+        depth = -(material / passes).ceilTo(2),
+        depthOverlay = 0.2,
+        emD = 25.4 / 8,
+        r = 3 + emD / 2,
+        dx = w - 2 * r + emD,
+        dy = h - 2 * r + emD,
+        d1 = 5.3,
+        d2 = 7.5;
+
+    // holes
+    var pos = [
+      [10.0,10.0,d1], [30.0,10.0,d1], [10.0,30.0,d1], [30.0,30.0,d1],
+      [10.0,50.0,d1], [30.0,50.0,d1], [10.0,70.0,d1], [30.0,70.0,d1],
+      [70.0,10.0,d2], [70.0,30.0,d2], [70.0,50.0,d2], [70.0,70.0,d2],
+    ];
+    for(hole in pos) {
+      for(i in 0...passes) {
+        po.travel()
+          .z(o)
+          .abs(hole[0], hole[1])
+          .z(0)
+          .mill(mill)
+          .z(depth * (1 + i) - depthOverlay)
+          .hole(emD, hole[2], 3);
+      }
+    }
+    po.travel()
+      .z(o)
+      .abs(-emD/2, r-emD/2)
+      .z(0)
+      .mill(mill);
+
+    // profile
+    for(_ in 0...passes) {
+      po.rz(depth)
+        .ry(dy)
+        .rarc(r, 0, r, r)
+        .rx(dx)
+        .rarc(0, -r, r, -r)
+        .ry(-dy)
+        .rarc(-r, 0, -r, -r)
+        .rx(-dx)
+        .rarc(0, r, -r, r);
+    }
+
+    po.z(o)
+      .abs(0, 0);
+  }
+}
